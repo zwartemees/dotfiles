1ae3aca89328e23e230f312403d596e0bf0a263f
zwartemees <zwartemees@gmail.com>
2025-06-04 08:56:53 +0200

    install.sh added

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "installing dependancies" 
+sudo apt install git curl 
+
+echo "installing xz" 
+git clone https://github.com/tukaani-project/xz
+cd xz
+Cmake .
+make
+sudo mv xz /usr/bin/
+cd ..
+rm -rf xz
+
+echo "installing nix..."
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+
+echo "installing home-manager" 
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+echo "cloning and installing dotfiles" 
+git clone https://github.com/zwartemees/dotfiles.git
+ln -sf ~/dotfiles/home-manager/home.nix ~/.config/home.nix
+home-manager switch
