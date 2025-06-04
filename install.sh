0676a743332e133fca1844cabd36159d92d7a756
zwartemees <zwartemees@gmail.com>
2025-06-04 10:47:01 +0200

    init support for real?

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "installing nix..."
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 . /etc/profile.d/nix.sh
 
-if [ "$(ps -p 1 -o comm=)" = "init" ]; then
+if [ "$(ps -p 1 -o comm=)" =~ "init" ]; then
     echo "System is not using init not systemd you might experience problems with nix"
     echo "trying to install daemon"
 
